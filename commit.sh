2a386686242700f6e1b15d0e2538d5b6b5079ab2
Julia Allyce <jpoladsky@gmail.com>
2016-05-19 23:04:47 -0400

    force push duh

--- commit.sh
+++ commit.sh
@@ -1,9 +1,11 @@
-  git init
-  git config user.name "Julia Allyce"
-  git config user.email ${EMAIL}
+git init
+git config user.name "Julia Allyce"
+git config user.email ${EMAIL}
+git remote add origin "https://${GITHUB_TOKEN}@${GITHUB_REPO}"
+git pull origin master
 
-  echo "lol" >> README.md
+echo "lol" >> README.md
 
-  git add README.md
-  git commit -m "killin' it"
-  git push "https://${GITHUB_TOKEN}@${GITHUB_REPO}" master
+git add README.md
+git commit -m "killin' it"
+git push origin master
